333662438afce9388aeb7cfc7c101303cbdccdfe
book <1214376534@qq.com>
2025-08-15 14:02:46 +0800

    perf-opt

--- src/gpu-kernal/fp16.cu
+++ src/gpu-kernal/fp16.cu
@@ -13,6 +13,17 @@ __global__ void fp16ToFp32Gpu(uint16_t* fp16_buffer, float* fp32_buffer, int val
     }
 }
 
+__global__ void fp16ToFp32GpuSmall(uint16_t* fp16_buffer, int val_num) {
+    int thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+    float *buf32 = (float*)fp16_buffer;
+    if (thread_id == 0) {
+        for (int j = val_num - 1; j >= 0; j--) {
+             __half fp16 = *(__half*)((fp16_buffer + j));
+             *(buf32 + j) = __half2float(fp16);
+        }
+    }
+}
+
 void lanuchFp16ToFp32Gpu(uint16_t* fp16_buffer, float* fp32_buffer, int val_num) {
     int block_size = 512;
     int total_num = val_num;
@@ -42,11 +53,15 @@ int lanuchFp16ToFp32GpuInplace(uint16_t* fp16_buffer, int val_num) {
         fp16ToFp32Gpu<<<grid_size, block_size>>>(fp16_buf, fp32_buf, cur_num);
         cudaDeviceSynchronize();
         cur_end = cur_start - 1;
-        //printf("Inplace_fp16_fp32, global_index: %d, cur_num: %d\n", global_index, cur_num);
         if (cur_end < 1000) {
-            //TODO move to cpu 
-            return cur_end;
+            break;
         }
     }
+    int cur_start = 0;
+    int grid_size = 1;
+    block_size = 32;
+    int cur_num = cur_end - cur_start + 1;
+    fp16ToFp32GpuSmall<<<grid_size, block_size>>>(fp16_buffer, cur_num);
+    cudaDeviceSynchronize();
     return -1;
 }
